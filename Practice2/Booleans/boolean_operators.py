ba09dd0ba4809d6b72a806399798e9f90e0d0813
Danil Kozlov <8c7zjt4j42@privaterelay.appleid.com>
2026-02-05 16:17:33 +0500

    Add Practice2

---
+++ Practice2/Booleans/boolean_operators.py
@@ -0,0 +1,6 @@
+a = True
+b = False
+
+print("True and False =", a and b)
+print("True or False =", a or b)
+print("not True =", not a)
---
+++ Practice2/While_loops/timer.py
@@ -0,0 +1,7 @@
+time = 5
+
+while time > 0:
+    print(time)
+    time = time - 1
+
+print("START!")
