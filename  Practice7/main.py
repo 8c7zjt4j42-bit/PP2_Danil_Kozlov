648fe828d7c0c65ec398249813b7004e36108419
Danil Kozlov <8c7zjt4j42@privaterelay.appleid.com>
2026-03-26 16:27:50 +0500

    update

---  Practice7/main.py
+++  Practice7/main.py
@@ -1,15 +1,26 @@
 import psycopg2
 import csv
+import os
 
 
 conn = psycopg2.connect(
     host="localhost",
     database="phonebook_db",
     user="postgres",
-    password="122101"  
+    password="122101"
 )
 
 cur = conn.cursor()
+
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS phonebook (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL
+);
+""")
+conn.commit()
 
 
 def add_user():
@@ -24,6 +35,7 @@ def add_user():
     print("User added")
 
 
+
 def show_all():
     cur.execute("SELECT * FROM phonebook ORDER BY id ASC")
     rows = cur.fetchall()
@@ -31,6 +43,7 @@ def show_all():
     print("\n--- PhoneBook ---")
     for row in rows:
         print(row)
+
 
 
 def search_user():
@@ -50,17 +63,63 @@ def search_user():
         print("User not found")
 
 
-def update_user():
-    name = input("Enter name to update: ")
-    new_phone = input("Enter new phone: ")
+
+def search_by_name_part():
+    part = input("Enter part of name: ")
 
     cur.execute(
-        "UPDATE phonebook SET phone = %s WHERE name = %s",
-        (new_phone, name)
+        "SELECT * FROM phonebook WHERE name ILIKE %s",
+        ("%" + part + "%",)
     )
-    conn.commit()
 
+    rows = cur.fetchall()
+
+    if rows:
+        for row in rows:
+            print(row)
+    else:
+        print("No matches")
+
+
+
+def search_by_prefix():
+    prefix = input("Enter phone prefix: ")
+
+    cur.execute(
+        "SELECT * FROM phonebook WHERE phone LIKE %s",
+        (prefix + "%",)
+    )
+
+    rows = cur.fetchall()
+
+    if rows:
+        for row in rows:
+            print(row)
+    else:
+        print("No matches")
+
+
+
+def update_user():
+    name = input("Enter name to update: ")
+    new_name = input("New name (press Enter to skip): ")
+    new_phone = input("New phone (press Enter to skip): ")
+
+    if new_name:
+        cur.execute(
+            "UPDATE phonebook SET name = %s WHERE name = %s",
+            (new_name, name)
+        )
+
+    if new_phone:
+        cur.execute(
+            "UPDATE phonebook SET phone = %s WHERE name = %s",
+            (new_phone, name)
+        )
+
+    conn.commit()
     print("Updated")
+
 
 
 def delete_user():
@@ -70,14 +129,17 @@ def delete_user():
         "DELETE FROM phonebook WHERE name = %s",
         (name,)
     )
-    conn.commit()
 
+    conn.commit()
     print("Deleted")
+
 
 
 def insert_from_csv():
     try:
-        with open("data.csv", "r") as file:
+        file_path = os.path.join(os.path.dirname(__file__), "data.csv")
+
+        with open(file_path, "r") as file:
             reader = csv.reader(file)
             next(reader)
 
@@ -100,11 +162,13 @@ while True:
     print("\n===== MENU =====")
     print("1. Add user")
     print("2. Show all users")
-    print("3. Search user")
-    print("4. Update user")
-    print("5. Delete user")
-    print("6. Insert from CSV")
-    print("7. Exit")
+    print("3. Search user (exact)")
+    print("4. Search by name (part)")
+    print("5. Search by phone prefix")
+    print("6. Update user")
+    print("7. Delete user")
+    print("8. Insert from CSV")
+    print("9. Exit")
 
     choice = input("Choose: ")
 
@@ -118,15 +182,21 @@ while True:
         search_user()
 
     elif choice == "4":
-        update_user()
+        search_by_name_part()
 
     elif choice == "5":
-        delete_user()
+        search_by_prefix()
 
     elif choice == "6":
-        insert_from_csv()
+        update_user()
 
     elif choice == "7":
+        delete_user()
+
+    elif choice == "8":
+        insert_from_csv()
+
+    elif choice == "9":
         break
 
     else:
